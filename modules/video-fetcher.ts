c5a846e623f99c05b4e0ac59d2f98fdf72f144e8
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-03 22:45:34 +0900

    동영상 가져오는 로직 수정
    - shorts 판별법 추가
    - 특정 시점 이후의 동영상만 가져오도록 수정
    - 가져오지 않을 동영상의 타입을 videoFetcher에서 바로 설장

--- modules/video-fetcher.ts
+++ modules/video-fetcher.ts
@@ -1,8 +1,12 @@
 import axios, { type AxiosResponse } from 'axios'
 import type { FetchedVideo, YoutubeVideo, YoutubeVideoCategoryList, YoutubeVideoList } from '../types';
+import { exec } from 'child_process'
 
 export default class VideoFetcher {
     private youtubeDataKey: string | undefined;
+    private enailRegex: RegExp = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+    private dayMillisec = 1000 * 60 * 60 * 24;
+
     constructor() {
         this.youtubeDataKey = process.env.YOUTUBE_DATA_API_KEY;
         if (!this.youtubeDataKey) return;
@@ -10,59 +14,99 @@ export default class VideoFetcher {
 
     fetchVideo = async (
         maxResults: number = 10, 
-        includeShorts: boolean = false
+        includeShorts: boolean = false, 
+        maxDuration: number = 1000,
+        notFetchFilter?: {categoryId: (number | string)[], title: string[]}
     ): Promise<FetchedVideo[]> => {
-        if (maxResults <= 0) maxResults = 10
+        if (maxResults <= 0) maxResults = 0
         else if (maxResults > 50) maxResults = 50
 
         let nextPageToken: string | undefined = "";
         let videos: YoutubeVideo[] = []
         do {
-            const response: AxiosResponse<YoutubeVideoList> = await axios.get<YoutubeVideoList>("https://www.googleapis.com/youtube/v3/videos", {
+            let { data }: { data: YoutubeVideoList } = await axios.get<YoutubeVideoList>("https://www.googleapis.com/youtube/v3/videos", {
                 params: {
                     ...this.defaultParams(maxResults, nextPageToken),
                 }
             })
-            videos.push(...response.data.items)
-            nextPageToken = response.data.nextPageToken
+            let items = data.items.filter(video => this.doNotFetchVideoFilter(video, notFetchFilter))
+            videos.push(...this.filterOldVideos(items, maxDuration))
+            nextPageToken = data.nextPageToken
+            data = null as any;
         } while(nextPageToken);
 
-        if (!includeShorts) videos = videos.filter(video => !this.isShortsVideo(video))
+        if (!includeShorts) await this.removeShortsVideoItem(videos)
 
         return this.generateCommonVideoDatas(videos);
     }
 
-    fetchVideoByCategoryId = async(videoCategoryId: string | number, maxResults: number, includeShorts: boolean = false): Promise<FetchedVideo[]> => {
-        if (maxResults <= 0) maxResults = 10
+    fetchVideoByCategoryId = async (
+        videoCategoryId: string | number, 
+        maxResults: number = 10, 
+        includeShorts: boolean = false, 
+        maxDuration: number = 1000,
+        notFetchFilter?: {categoryId: (number | string)[], title: string[]}
+    ): Promise<FetchedVideo[]> => {
+        if (maxResults <= 0) maxResults = 0
         else if (maxResults > 50) maxResults = 50
         let nextPageToken: string | undefined = "";
         let videos: YoutubeVideo[] = []
         do {
-            const response: AxiosResponse<YoutubeVideoList> = await axios.get<YoutubeVideoList>("https://www.googleapis.com/youtube/v3/videos", {
+            let { data }: { data: YoutubeVideoList } = await axios.get<YoutubeVideoList>("https://www.googleapis.com/youtube/v3/videos", {
                 params: {
                     ...this.defaultParams(maxResults, nextPageToken),
                     videoCategoryId: `${videoCategoryId}`,
                 }
             })
-            videos.push(...response.data.items)
-            nextPageToken = response.data.nextPageToken
+            let items = data.items.filter(video => !this.doNotFetchVideoFilter(video, notFetchFilter))
+            videos.push(...this.filterOldVideos(items, maxDuration))
+            nextPageToken = data.nextPageToken
+            data = null as any;
         } while(nextPageToken);
         
-        if (!includeShorts) videos = videos.filter(video => !this.isShortsVideo(video))
+        if (!includeShorts) videos = await this.removeShortsVideoItem(videos)
 
         return this.generateCommonVideoDatas(videos)
     }
 
+    private doNotFetchVideoFilter = (video: YoutubeVideo, notFetchFilter?: {categoryId: (number | string)[], title: string[]}) => {
+        const videoTitle = video.snippet.title.toLowerCase();
+        let notFetchFilterFlag = false
+        const isKoreanInclude = /[가-힣]+/.test(videoTitle);
+        if (notFetchFilter) {
+            const { categoryId: categoryIds, title: titles } = notFetchFilter;
+            notFetchFilterFlag = categoryIds?.includes(video.snippet.categoryId) ||
+                                    titles?.some(title => videoTitle.includes(title))
+        }
+        return !isKoreanInclude || notFetchFilterFlag;
+    }
+
+    private filterOldVideos = (videos: YoutubeVideo[], daysLimit: number) => {
+        return videos.filter(video => {
+            const millisecDiff = new Date().setHours(0, 0, 0, 0) - new Date(video.snippet.publishedAt).setHours(0, 0, 0, 0)
+            return millisecDiff / this.dayMillisec <= daysLimit
+        })
+    }
+
+    private removeShortsVideoItem = async (videos: YoutubeVideo[]) => {
+        return (await Promise.all(
+            videos.map(async (video) => {
+                const isShortsVideo = await this.isShortsVideo(video)
+                return isShortsVideo ? null : video
+            })
+        )).filter(video => video !== null)
+    }
+
     private generateCommonVideoDatas = (videos: YoutubeVideo[]): FetchedVideo[] => videos.map(video => ({
-            id: video.id,
-            title: video.snippet.title,
-            thumbnail: video.snippet.thumbnails.medium.url,
-            description: video.snippet.description,
-            channelId: video.snippet.channelId,
-            channelTitle: video.snippet.channelTitle,
-            publishedAt: video.snippet.publishedAt,
-            categoryId: video.snippet.categoryId,
-        }));
+        id: video.id,
+        title: video.snippet.title,
+        thumbnail: video.snippet.thumbnails.medium.url,
+        description: video.snippet.description,
+        channelId: video.snippet.channelId,
+        channelTitle: video.snippet.channelTitle,
+        publishedAt: video.snippet.publishedAt,
+        categoryId: video.snippet.categoryId,
+    }));
 
     private defaultParams = (maxResults: number, nextPageToken: string) => ({
         key: this.youtubeDataKey,
@@ -73,11 +117,43 @@ export default class VideoFetcher {
         pageToken: nextPageToken,
     })
 
-    private isShortsVideo = (video: YoutubeVideo): boolean => {
-        const isUnderMinutes = this.convertISO8601ToSecond(video.contentDetails.duration) <= 120 // 2분
-        const isDescriptionIncludesHashtag = /#shorts/gi.test(video.snippet.description)
-        return isUnderMinutes || isDescriptionIncludesHashtag;
+    private isShortsVideo = async (video: YoutubeVideo): Promise<boolean> => {
+        const videoRatio = await this.getVideoResolutionRatio(video.id);
+        const isUnderMinutes = this.convertISO8601ToSecond(video.contentDetails.duration) <= 3 * 60 // 3분
+        return isUnderMinutes && videoRatio <= 1;
     };
+
+    private getVideoResolutionRatio = async (videoId: string): Promise<number> => {
+        return new Promise((resolve, reject) => {
+            if (!videoId) reject('plz input youtube videoId')
+            exec(`yt-dlp -F 'https://www.youtube.com/watch?v=${videoId}'`, (err, stdout, stderr) => {
+                if (err) reject(`exec error: ${err}`);
+                if (stderr) reject(`stderr: ${stderr}`);
+
+                const notStartsWith = ['[', '---', 'ID']
+
+                const lines = stdout.trim().split('\n').filter(
+                    line => line && !notStartsWith.some(prefix => line.startsWith(prefix))
+                )
+                
+                const results = []
+                for (let line of lines) {
+                    const [id, ext, resolution, ...rest] = line.split(/\s+/)
+                    if (!['webm', 'mp4'].includes(ext)) continue;
+                    if (resolution === 'audio') continue;
+                    results.push({id, ext, resolution})
+                }
+    
+                const [width, height] = results.at(-1)?.resolution.toLowerCase().split('x') || []
+                if (!width || !height) reject('video resolution not found');
+    
+                const videoRatio = Number(width) / Number(height)
+                if (Number.isNaN(videoRatio)) reject('video resolution calculation result is NaN')
+    
+                resolve(videoRatio)
+            })
+        })
+    }
 
     private convertISO8601ToSecond = (duration: string): number => {
         const matched = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/)

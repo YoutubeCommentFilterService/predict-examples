4f8a2728c5c241cf381a0fd5ed6173cc8c17dcd2
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-07 16:07:24 +0900

    변수명 변경 및 return value 추가

--- modules/comment-predictor.ts
+++ modules/comment-predictor.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import type { ExtractedComment, PredictResponse, PredictResult, SpamContent } from '../types';
+import type { ExtractedComment, PredictResponse, PredictResult, SpamContent, SpamResult } from '../types';
 import fs from 'fs';
 import appRootPath from 'app-root-path';
 import { seperator } from './utils';
@@ -25,45 +25,44 @@ export default class CommentPredictor {
                 nickname: data.nickname,
                 comment: data.translatedText,
             }))
+            const { data } = await axios.post<PredictResponse>(`${this.serverUrl}/predict`, { items })
+            const { comment_categories: commentCategories, nickname_categories: nicknameCategories } = data;
 
-            const response = await axios.post<PredictResponse>(`${this.serverUrl}/predict`, {
-                items
-            })
-
-            const data = response.data
-            const comment_categories = data.comment_categories;
-            const nickname_categories = data.nickname_categories;
+            // 좀 더 안정적이려면 id를 비교해서 가져오는게 최고.
+            // 하지만 요청때 데이터를 추론하기 때문에 크게 문제가 없을 것이다..!
             data.items.forEach((item, idx) => {
-                const nickname_predicted = item.nickname_predicted;
-                const comment_predicted = item.comment_predicted;
-                const spamFlag = nickname_predicted === '스팸' || comment_predicted === '스팸'
-                const rankFlag = nickname_predicted === '정상' && comment_predicted === '순위'
-                const begFlag = nickname_predicted === '정상' && comment_predicted === '구걸'
-                const nSeeFlag = nickname_predicted === '정상' && comment_predicted === '안봄'
+                const { nickname_predicted: nicknamePredicted, comment_predicted: commentPredicted } = item;
+                const isSpamComment = nicknamePredicted === '스팸' || commentPredicted === '스팸'
+                const isNormalNickname = nicknamePredicted === '정상'
+                const isRankComment = isNormalNickname && commentPredicted === '순위'
+                const isBegComment = isNormalNickname && commentPredicted === '구걸'
+                const isNSeeComment = isNormalNickname && commentPredicted === '안봄'
 
-                const nickname_prob = item.nickname_predicted_prob.map((data, idx) => `${nickname_categories[idx]}(${String(Math.floor(data*100)).padStart(3, ' ')}%)`).join(', ')
-                const comment_prob = item.comment_predicted_prob.map((data, idx) => `${comment_categories[idx]}(${String(Math.floor(data*100)).padStart(3, ' ')}%)`).join(', ')
+                const nicknameProb = item.nickname_predicted_prob.map((data, idx) => `${nicknameCategories[idx]}(${String(Math.floor(data*100)).padStart(3, ' ')}%)`).join(', ')
+                const commentProb = item.comment_predicted_prob.map((data, idx) => `${commentCategories[idx]}(${String(Math.floor(data*100)).padStart(3, ' ')}%)`).join(', ')
 
                 const comment = originDatas[idx].originalText
 
                 const spamResult = {
-                    'nickname': originDatas[idx].nickname,
-                    'comment': comment,
-                    'id': item.id,
-                    'profileImage': originDatas[idx].profileImage,
-                    nickname_predicted,
-                    comment_predicted,
-                    nickname_prob,
-                    comment_prob
-                }
-                if (spamFlag) predictSpamResults.push(spamResult)
+                    nickname: originDatas[idx].nickname,
+                    comment,
+                    id: item.id,
+                    profileImage: originDatas[idx].profileImage,
+                    nicknamePredicted,
+                    commentPredicted,
+                    nicknameProb,
+                    commentProb,
+                    parentId: originDatas[idx].parentId,
+                    updatedAt: new Date(originDatas[idx].updatedAt).toLocaleString('ko-KR', { "timeZone": "Asia/Seoul"})
+                } as SpamContent;
+                if (isSpamComment) predictSpamResults.push(spamResult)
                 else if (debug) {
                     const crToSpace = comment.replace(/\r/g, '').replace(/\n/g, '   ')
                     const removeGolbangE = originDatas[idx].nickname.substring(1)
-                    const writeData = `${nickname_prob}, ${removeGolbangE}\n\t${comment_prob}, ${crToSpace}`
-                    if (rankFlag) predictCommonResults['rank'].push(writeData)
-                    else if (begFlag) predictCommonResults['beg'].push(writeData)
-                    else if (nSeeFlag) predictCommonResults['nsee'].push(writeData)
+                    const writeData = `${nicknameProb}, ${removeGolbangE}\n\t${commentProb}, ${crToSpace}`
+                    if (isRankComment) predictCommonResults['rank'].push(writeData)
+                    else if (isBegComment) predictCommonResults['beg'].push(writeData)
+                    else if (isNSeeComment) predictCommonResults['nsee'].push(writeData)
                     else predictCommonResults['normal'].push(writeData)
                 }
             })
@@ -80,7 +79,7 @@ export default class CommentPredictor {
 
     private saveAsSpam = (videoId: string, predictSpamResults: SpamContent[]): void => {
         const fname = `${appRootPath}/predicts/${videoId}.spam.txt`
-        const writeData = predictSpamResults.map(result => `${result.profileImage}\n\t(${result.nickname_predicted}), ${result.nickname_prob} - ${result.nickname}\n\t${result.comment_prob} - ${result.comment.replaceAll('\r\n', ' ').replaceAll('\n', ' ')}`).join('\n')
+        const writeData = predictSpamResults.map(result => `${result.profileImage}\n\t(${result.nicknamePredicted}), ${result.nicknameProb} - ${result.nickname}\n\t${result.commentProb} - ${result.comment.replaceAll('\r\n', ' ').replaceAll('\n', ' ')}`).join('\n')
         fs.writeFileSync(fname, writeData + '\n')
     }
 

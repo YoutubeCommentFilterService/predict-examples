9f1d43807d553cd047f4a536ae73690a978fcf30
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-07 16:05:07 +0900

    메일 템플릿 버전에 맞게 갈아끼울 수 있도록 수정

--- modules/mailer-service.ts
+++ modules/mailer-service.ts
@@ -1,13 +1,15 @@
-import dotenv from 'dotenv';
 import nodemailer from 'nodemailer';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
 import ejs from 'ejs';
-import fs from 'fs';
-import type { SendMailData } from '../types';
+import appRootPath from 'app-root-path';
 
 export default class MailerService {
     private transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>;
-    private templatePath: string;
+    private mainTemplatePath: string = `${appRootPath}/static/spam-comment-email-template.ejs`
+    private templatePath: {[key: string]: string} = {
+        v1: `${appRootPath}/static/v1.ejs`,
+        v2: `${appRootPath}/static/v2.ejs`
+    }
     constructor() {
         this.transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -16,43 +18,39 @@ export default class MailerService {
                 pass: process.env.MAILER_PASS,
             },
         })
-        this.templatePath = "../static/spam-comment-email-template.ejs";
     }
 
     private truncateString = (str: string, maxLength: number) => {
         return str.length > maxLength ? str.substring(0, maxLength) + '...' : str;
     }
 
-    sendMail = async (to: string, data: SendMailData): Promise<void> => {
-        // if (data.comments.length === 0) {
-        //     console.log('스팸이 감지되지 않았습니다')
-        //     return;
-        // }
+    private generateMailOptions = (to: string, subject: string, html: string) => ({
+        from: process.env.MAILER_USER,
+        to,
+        subject,
+        html,
+        headers: {
+            'X-Mailer': 'Nodemailer',
+            'X-Priority': '3',
+            'Precedence': 'bulk'
+        }
+    })
 
+    sendMail = async (to: string, data: any, version: string = 'v1'): Promise<void> => {
         try {
             const title = `${this.truncateString(data.video.title, 20)} 영상에 의심스러운 댓글이 감지되었습니다.`
-            const template = await this.renderTemplate(data)
-            const mailOptions = {
-                from: process.env.MAILER_USER,
-                to,
-                subject: title,
-                html: template,
-                headers: {
-                    'X-Mailer': 'Nodemailer',
-                    'X-Priority': '3',
-                    'Precedence': 'bulk'
-                },
-            }
-
+            const mailBody = await this.renderTemplate(this.templatePath[version], data)
+            const template = await this.renderTemplate(this.mainTemplatePath, { video: data['video'], partialBodyTemplate: mailBody, version })
+            const mailOptions = this.generateMailOptions(to, title, template)
             const info = await this.transporter.sendMail(mailOptions);
         } catch (err) {
             console.log(err);
         }
     }
 
-    private renderTemplate = async (data: SendMailData): Promise<string> => {
+    private renderTemplate = async (ejsPath: string, data: any, options: any = {}): Promise<string> => {
         return new Promise((resolve, reject) => {
-            ejs.renderFile(this.templatePath, { comments: data.comments, video: data.video }, (err, htmlContent) => {
+            ejs.renderFile(ejsPath, data, options, (err, htmlContent) => {
                 if (err) reject('Error rendering EJS template: ' + err)
                 resolve(htmlContent);
             })

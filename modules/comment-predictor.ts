a9d15f3173f288156d05c585e7f1e72c69dbed23
PromotezCitizen <gkstkdgus821@gmail.com>
2025-08-17 17:53:44 +0900

    type map을 활용한 변수 수 줄임 및 stastics 추가

--- modules/comment-predictor.ts
+++ modules/comment-predictor.ts
@@ -1,15 +1,22 @@
 import axios from 'axios';
-import type { ExtractedComment, PredictResponse, PredictResult, SpamContent, SpamResult } from '../types';
+import type { ExtractedComment, PredictResponse, PredictResult, SpamContent, SpamPredictResult, SpamResult } from '../types';
 import fs from 'fs';
 import appRootPath from 'app-root-path';
 
 export default class CommentPredictor {
     private serverUrl: string | undefined;
+    private typeMap: { [key: string]: string } = {
+        '순위': 'rank',
+        '구걸': 'beg',
+        '안봄': 'nsee',
+        '요청/질문': 'rq',
+        '정치/비난': 'poli',
+    }
     constructor() {
         this.serverUrl = process.env.PREDICT_SERVER_URL;
     }
 
-    predictComment = async (originDatas: ExtractedComment[], videoId: string, debug: boolean = false): Promise<SpamContent[]> => {
+    predictComment = async (originDatas: ExtractedComment[], videoId: string, debug: boolean = false): Promise<SpamPredictResult> => {
         const predictSpamResults: SpamContent[] = [];
         const predictResults: { [key: string]: string[]} = {
             'normal': [],
@@ -18,6 +25,7 @@ export default class CommentPredictor {
             'nsee': [],
             'spam': [],
             'rq': [],
+            'poli': [],
         }
         try {
             const items = originDatas.map((data) => ({
@@ -31,43 +39,32 @@ export default class CommentPredictor {
             // 하지만 요청때 데이터를 추론하기 때문에 크게 문제가 없을 것이다..!
             data.items.forEach((item, idx) => {
                 const { nickname_predicted: nicknamePredicted, comment_predicted: commentPredicted } = item;
-                const isSpamComment = nicknamePredicted === '스팸' || commentPredicted === '스팸'
-                const isNormalNickname = nicknamePredicted === '정상'
-                const isRankComment = isNormalNickname && commentPredicted === '순위'
-                const isBegComment = isNormalNickname && commentPredicted === '구걸'
-                const isNSeeComment = isNormalNickname && commentPredicted === '안봄'
-                const isReQuest = isNormalNickname && commentPredicted === '요청/질문'
+                const isSpamContent = nicknamePredicted === '스팸' || commentPredicted === '스팸'
+                const dictKey = isSpamContent ? 'spam' : this.typeMap[commentPredicted] || 'normal'
+                const isPoliticOrblameContent = dictKey === 'poli'
 
                 const nicknameProb = item.nickname_predicted_prob.map((data, idx) => `${nicknameCategories[idx]}(${String(Math.floor(data*100)).padStart(3, ' ')}%)`).join(', ')
                 const commentProb = item.comment_predicted_prob.map((data, idx) => `${commentCategories[idx]}(${String(Math.floor(data*100)).padStart(3, ' ')}%)`).join(', ')
 
                 const comment = originDatas[idx].originalText
 
-                const spamResult = {
-                    nickname: originDatas[idx].nickname,
-                    comment,
-                    id: originDatas[idx].id,
-                    profileImage: originDatas[idx].profileImage,
-                    nicknamePredicted,
-                    commentPredicted,
-                    nicknameProb,
-                    commentProb,
-                    parentId: originDatas[idx].parentId,
-                    updatedAt: new Date(originDatas[idx].updatedAt).toLocaleString('ko-KR', { "timeZone": "Asia/Seoul"})
-                } as SpamContent;
-                
                 const crToSpace = comment.replace(/\r/g, '').replace(/\n/g, '  ')
                 const writeData = `${originDatas[idx].profileImage}\n\t${originDatas[idx].nickname} - ${originDatas[idx].likes}, https://youtube.com/${originDatas[idx].nickname}\n\t${nicknameProb}\n\t${commentProb}\n\t${crToSpace}`
-                if (isSpamComment) {
-                    predictResults['spam'].push(writeData)
+                predictResults[dictKey].push(writeData);
+                if (isSpamContent || isPoliticOrblameContent) {
+                    const spamResult = {
+                        nickname: originDatas[idx].nickname,
+                        comment,
+                        id: originDatas[idx].id,
+                        profileImage: originDatas[idx].profileImage,
+                        nicknamePredicted,
+                        commentPredicted,
+                        nicknameProb,
+                        commentProb,
+                        parentId: originDatas[idx].parentId,
+                        updatedAt: new Date(originDatas[idx].updatedAt).toLocaleString('ko-KR', { "timeZone": "Asia/Seoul"})
+                    } as SpamContent;
                     predictSpamResults.push(spamResult)
-                }
-                else if (debug) {
-                    if (isRankComment) predictResults['rank'].push(writeData)
-                    else if (isBegComment) predictResults['beg'].push(writeData)
-                    else if (isNSeeComment) predictResults['nsee'].push(writeData)
-                    else if (isReQuest) predictResults['rq'].push(writeData)
-                    else predictResults['normal'].push(writeData)
                 }
             })
         } catch (err) {
@@ -75,10 +72,18 @@ export default class CommentPredictor {
             else console.error(err)
         }
         Object.keys(predictResults).forEach(key => {
-            if (!debug && key !== "spam") return;
+            if (!debug && !(key === 'spam' || key === 'poli')) return;
             if (predictResults[key].length > 0) this.saveResult(key, videoId, predictResults[key])
         })
-        return predictSpamResults;
+        const spamPredictResult: SpamPredictResult = {
+            result: predictSpamResults,
+            statistic: {
+                total: originDatas.length || 0,
+                spam: Object.values(predictResults['spam']).length || 0,
+                politic: Object.values(predictResults['poli']).length || 0
+            }
+        }
+        return spamPredictResult;
     }
 
     private saveResult = (type: string, videoId: string, datas: string[]): void => {

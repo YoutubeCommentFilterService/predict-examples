bbb7bc3da0477da02f26b1aa2520d2e8d236b7d4
PromotezCitizen <gkstkdgus821@gmail.com>
2025-09-02 20:33:34 +0900

    axios youtube fetcher 로직으로 변환

--- datas/find-emails.ts
+++ datas/find-emails.ts
@@ -3,34 +3,36 @@ import appRootPath from 'app-root-path';
 import axios, { isAxiosError } from 'axios';
 import dotenv from 'dotenv';
 import { seperator } from '../modules/utils';
-import fetchChannelData from './find-email-helper/fetch-bio';
 import { EMAIL_LIST_FILE_PATH, EMAIL_REGEX, MAX_FETCH_VIDEO_LENGTH, NOT_FOUND_EMAIL_LIST_FILE_PATH, RETRY_EMAIL_LIST_FILE_PATH, SEARCH_EMAIL_LIST_FILE_PATH } from './find-email-helper/constants';
-import { closeBrowser, crawlingStart, openBrowser } from './find-email-helper/crawling-youtube';
+
 import spinnerCarousel from './find-email-helper/spinner-carousel';
-import type { FetchPlaylistItemResult, YoutubeChannelInfo, YoutubePlaylistItemResponse, YoutubeVideoResponse } from './types/youtube';
+import type { FetchPlaylistItemResult, YoutubeChannelInfo, YoutubeChannelUUIDInfo, YoutubePlaylistItemResponse, YoutubeVideoResponse } from './types/youtube';
 
 function clearCraousel(carouselId: number | Timer) {
     clearInterval(carouselId)
     console.log()
 }
 
+const youtubeChannelEmailRecord: Record<string, YoutubeChannelInfo> = {}
+const failed: YoutubeChannelUUIDInfo[] = []
 dotenv.config({ path: `${appRootPath}/env/.env` })
+/* ============================== 이전 버전은 직접 브라우저 크롤링으로 했다 ==============================
+import { closeBrowser, crawlingStart, openBrowser } from './find-email-helper/crawling-youtube';
+import { fetchChannelData } from './find-email-helper/fetch-bio';
 
 // 채널 이름, 상세 페이지 json id 추출
 const browser = await openBrowser()
 const { succeed, failed } = await crawlingStart(browser)
 await closeBrowser(browser)
 
-const youtubeChannelEmailRecord: Record<string, YoutubeChannelInfo> = succeed.reduce((prev, coming) => {
-    prev[coming.channelId] = {
-        channelName: coming.channelName ?? '',
+for (const data in succeed) {
+    youtubeChannelEmailRecord[data.channelId] = {
+        channelName: data.channelName ?? '',
         channelHandler: '',
-        uuid: coming.uuid ?? '',
+        uuid: data.uuid ?? '',
         emails: []
     }
-    return prev
-}, {} as Record<string, YoutubeChannelInfo>)
-
+}
 console.log(`${succeed.length + failed.length} of ${succeed.length} succeed`)
 
 // uuid를 이용하여 채널 설명, 링크들에서 email 추출
@@ -44,6 +46,51 @@ bioResults.filter(res => res.emails).forEach(res => {
     data.emails = [ ...data.emails, ...res.emails ?? [] ]
 })
 clearCraousel(spinnerOfFetchDescriptionJson)
+*/ // =============================================================================================
+
+// ============================== 이제는 axios로 대체 ==============================
+import { fetchYoutubeBio } from './find-email-helper/axios-youtube';
+import { fetchSiteChannelDescription } from './find-email-helper/fetch-bio';
+let retryFailedIds: string[] = []
+let toSearchIds: string[] = []
+try {
+    retryFailedIds = fs.readFileSync(RETRY_EMAIL_LIST_FILE_PATH, { encoding: 'utf-8' }).split('\n').filter(Boolean).map(x => x.trim().replace(/\,/g, ''))
+} catch (e) {}
+try {
+    toSearchIds = fs.readFileSync(SEARCH_EMAIL_LIST_FILE_PATH, { encoding: 'utf-8' }).split('\n').filter(Boolean).splice(1).map(x => x.trim())
+} catch (e) {}
+const targetIds: string[] = [...retryFailedIds, ...toSearchIds]
+console.log(targetIds)
+
+const spinnerOfFetchDescriptionJson = spinnerCarousel('FETCH DESCRIPTION JSON')
+const bioInfos = await Promise.all(targetIds.map(fetchYoutubeBio))
+const resultMaps = bioInfos.map(async (data) => {
+    const emails = (data?.description || '').match(EMAIL_REGEX) || []
+    const promiseResults = (await Promise.all(data?.externalLinks.map(link => link.startsWith('http') ? link : `https://${link}`).map(fetchSiteChannelDescription))).flat()
+    return {
+        channelId: data.channelId,
+        channelHandler: data.handler,
+        channelName: data.channelName,
+        emails: [...new Set([...emails, ...promiseResults])]
+    }
+})
+const results = await Promise.all(resultMaps)
+for (const result of results) {
+    if (result.channelHandler) {
+        youtubeChannelEmailRecord[result.channelId] = {
+            channelName: result.channelName,
+            channelHandler: result.channelHandler,
+            emails: [...result.emails]
+        }
+    } else {
+        failed.push({
+            channelId: result.channelId
+        })
+    }
+}
+clearCraousel(spinnerOfFetchDescriptionJson)
+
+// ===============================================================================
 
 // 최신 동영상 id 추출
 const videoIds: string[] = []
@@ -124,9 +171,11 @@ const emailNotFoundDatas = Object.entries(youtubeChannelEmailRecord).filter(obj 
 
 const toLogText = (arr: string[]) => arr.length === 0 ? '' : arr.join('\n') + '\n'
 
-await Promise.all([
+await Promise.allSettled([
     fs.promises.appendFile(EMAIL_LIST_FILE_PATH, toLogText(emailFoundDatas)),
     fs.promises.appendFile(NOT_FOUND_EMAIL_LIST_FILE_PATH, toLogText(emailNotFoundDatas)),
-    fs.promises.appendFile(RETRY_EMAIL_LIST_FILE_PATH, toLogText(failed.map(item => item.channelId))),
-    fs.promises.rm(SEARCH_EMAIL_LIST_FILE_PATH)
+    fs.promises.writeFile(RETRY_EMAIL_LIST_FILE_PATH, toLogText(failed.map(item => item.channelId))),
+    fs.promises.rm(SEARCH_EMAIL_LIST_FILE_PATH, { force: true })
 ])
+
+console.log('append and remove files done')

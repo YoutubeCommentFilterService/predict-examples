c6addfde862e71fe734679b7dc2311c717ba02fd
PromotezCitizen <gkstkdgus821@gmail.com>
2025-09-10 20:36:59 +0900

    상수 재정의

--- datas/find-email-helper/constants.ts
+++ datas/find-email-helper/constants.ts
@@ -1,12 +1,16 @@
 import appRootPath from "app-root-path";
+import path from 'path';
 
 export const MAX_THREAD_SIZE = 16
 export const MAX_RETRY_COUNT = 5
-export const DATA_PATH = `${appRootPath}/datas`
-export const SEARCH_EMAIL_LIST_FILE_PATH = `${DATA_PATH}/to-search-emails.txt`
-export const RETRY_EMAIL_LIST_FILE_PATH = `${DATA_PATH}/retry-failed-emails.txt`
-export const SKIP_EMAIL_LIST_FILE_PATH = `${DATA_PATH}/skip-emails.txt`
-export const NOT_FOUND_EMAIL_LIST_FILE_PATH = `${DATA_PATH}/not-found-emails.txt`
-export const EMAIL_LIST_FILE_PATH = `${DATA_PATH}/emails.txt`
+export const DATA_PATH = path.join(appRootPath.path, 'datas')
+export const SEARCH_EMAIL_LIST_FILE_PATH    = path.join(DATA_PATH, 'to-search-emails.txt')
+export const RETRY_EMAIL_LIST_FILE_PATH     = path.join(DATA_PATH, 'retry-failed-emails.txt')
+export const SKIP_EMAIL_LIST_FILE_PATH      = path.join(DATA_PATH, 'skip-emails.txt')
+export const NOT_FOUND_EMAIL_LIST_FILE_PATH = path.join(DATA_PATH, 'not-found-emails.txt')
+export const EMAIL_LIST_FILE_PATH           = path.join(DATA_PATH, 'emails.txt')
+export const ALREADY_PREDICTED_VIDEOS_LIST_PATH = path.join(DATA_PATH, 'already-predicted.txt')
 export const EMAIL_REGEX = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g
 export const MAX_FETCH_VIDEO_LENGTH = 50
+
+export const PREDICT_RESULT_PATH = path.join(appRootPath.path, 'predict-results')
